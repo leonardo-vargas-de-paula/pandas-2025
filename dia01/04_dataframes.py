5032ec6f7014c0501d00fe936dd21b2fbbc5ab0d
Leonardo Vargas <leonardovargasdpaula@gmail.com>
2025-04-17 18:11:22 -0400

    feat:liçao 4

---
+++ dia01/04_dataframes.py
@@ -0,0 +1,41 @@
+# %%
+import pandas as pd
+
+idades = [32, 38, 30, 30, 31,
+          35, 25, 29, 31, 37,
+          27, 23, 36, 33, 39
+          ]
+
+nomes = [
+    "Téo", "Maria", "Jose", "Luis", "Ana",
+    "Nah", "Dani", "Mah", "Fer", "Nanda",
+    "Naty", "Nih", "Pedro", "Kozato", "Kozato",
+]
+
+series_idades = pd.Series(idades)
+series_nomes = pd.Series(nomes)
+
+# %%
+
+df = pd.DataFrame()
+
+df["idades"] = series_idades
+df["nomes"] = series_nomes
+df
+# %%
+
+type(df)
+# %%
+
+df["nomes"]
+# %%
+
+df.iloc[0]
+
+# %%
+df.iloc[0]["nomes"]
+# %%
+df.iloc[-1]
+# %%
+df.iloc[-1]["nomes"]
+# %%
